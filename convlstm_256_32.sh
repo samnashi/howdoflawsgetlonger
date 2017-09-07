b591a8a773ac8a0b3d863c90da57ac42e34932d2
Timotius Devin <timotiusdevin@live.com>
2017-09-08 00:49:28 +0700

    Update convlstm_256_32.sh

--- convlstm_256_32.sh
+++ convlstm_256_32.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "convlstm for Batch Size = 256 and Generator Pad Size = 32"
 echo "running the high learning rate, 300 draw sequence"
-python convlstm_1024_32_HLR.py
+python convlstm_256_32_HLR.py
 echo "HLR done"
 echo "running the medium learning rate, 300 draw sequence"
-python convlstm_1024_32_MLR.py
+python convlstm_256_32_MLR.py
 echo "MLR done"
 echo "running the low learning rate, 600 draw sequence"
-python convlstm_1024_32_LLR.py
+python convlstm_256_32_LLR.py
 echo "LLR done"
 echo "fin."
